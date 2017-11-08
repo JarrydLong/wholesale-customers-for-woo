053dd8e21ec42014faeea8bcd93841664ef74ef4
YooHooDev <dev@yoohooplugins.com>
2017-11-08 11:24:32 +0200

    Bug Fix and Subscription Popup Added
    - Bug Fix: If variations had same price for min and max value, display only one price instead of variation price.
    - Enhancement: Subscribe to our newsletter prompt

--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
-=== Wholesale Customers For WooCommerce ===
+﻿=== Wholesale Customers For WooCommerce ===
 Contributors: andrewza, yoohooplugins, travislima
 Tags: woocommerce wholesale, wholesale plugin, wholesale prices, wholesale pricing, woocommerce wholesale pricing, woocommerce, wholesale
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4GC4JEZH7KSKL
-Requires at least: 4.0
+Requires at least: 4.6
 Tested up to: 4.8
 Requires PHP: 5.6
 Stable tag: trunk
@@ -12,20 +12,30 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 A simple and easy to use B2B solution for WooCommerce. Wholesale Customers For Woo will assist you in selling wholesale through your WooCommerce store.
 
 == Description ==
+
 A simple and easy to use B2B solution for WooCommerce. Wholesale Customers For Woo will assist you in selling wholesale through your WooCommerce store.
+
 
 With a variety of features in the free version, you won’t need another wholesale plugin for WooCommerce. 
 
 Actively updated for features, bug fixes and more! 
 
+
+
 == Features ==
 
+
 * Assign user’s to be wholesale customers. (This may be used for VIP guests for WooCommerce).
+
 * Global discount (percentage) for WooCommerce store.
 * Minimum purchase amount ($) for wholesale customers.
+
 * Wholesale pricing for each product (simple products and/or variations). Overwrites the global discount if wholesale price is set within a particular product.
 
+* [Translation ready](https://translate.wordpress.org/projects/wp-plugins/wholesale-customers-for-woo)
+
 = Upcoming Features =
+
 These features will be released in any order and may change according to our discretion.
 
 * Minimum/Maximum quantity per product.
@@ -45,6 +55,7 @@ These features will be released in any order and may change according to our dis
 * Email functionality for all wholesale customers.
 * List of all wholesale customers in one place.
 * Shipping methods based on wholesale customer.
+* Payment methods based on wholesale customer.
 * Import / Export products for WooCommerce (including custom product meta).
 * More features to be listed…
 
@@ -57,14 +68,15 @@ We currently offer three channels for support:
 
 We will try our best to assist you to the best of our ability.
 
-
 == Installation ==
+
 1. Upload the plugin files to the \'/wp-content/plugins\' directory, or install the plugin through the WordPress plugins screen directly.
 2. Activate the plugin through the \'Plugins\' screen in WordPress.
 3. Navigate to \'WooCommerce\' and data will be showing under \'General Settings Tab\' > \'Pricing Options\'.
 4. To enable users to become wholesalers, simply enable the \'Wholesale Customer\' checkbox in the \'Edit User Profile\'.
 
 == Frequently Asked Questions ==
+
 = What version of WooCommerce does this support? =
 This has been tested on version 3.0 and up.
 
@@ -72,28 +84,46 @@ This has been tested on version 3.0 and up.
 If your product has a wholesale price set, this will overwrite the global discount.
 
 == Screenshots ==
+
 1. Set user to be a Wholesale Customer inside the user’s edit profile page.
 2. Set the global discount on your WooCommerce Store for wholesale customers.
 3. Minimum cart totals for WooCommerce checkout for wholesale customers.
 4. WooCommerce Simple product wholesale pricing.
 
 == Changelog ==
+
+= 1.0.4.1 =
+* Bug Fix: If variations had same price for min and max value, display only one price instead of variation price.
+* Enhancement: Subscribe to our newsletter prompt
+
+= 1.0.4 =
+* Enhancement: Translation support for GlotPress.
+* Bug Fix: Wholesale amounts would round-off.
+
 = 1.0.3 =
+
 * General Bug Fixes
 * Enhancement: Wholesale pricing for variation products ( Includes the pricing range to work with global discount + wholesale pricing at the same time).
 
 = 1.0.2 =
+
 * Enhancement: Wholesale pricing for simple product.
 
 = 1.0.1 =
+
 * Feature: minimum cart total for wholesale customers
 * Enhancement: Better UI for settings page.
 * Enhancement: Better coding structure for settings page.
 
 = 1.0.0 =
+
 * Initial Release.
 
 == Upgrade Notice ==
+
+= 1.0.4 =
+* Please upgrade Wholesale Customers For WooCommerce to receive bug fixes and new features.
+
 = 1.0.3 =
 * Please upgrade for new features and bug fixes.
 
---
+++ wholesale-customers-for-woo.php
@@ -0,0 +1,347 @@
+<?php
+/**
+ * Plugin Name: Wholesale Customers For Woo
+ * Description: Allow wholesale pricing for WooCommerce.
+ * Author: YooHoo Plugins
+ * Author URI: https://yoohooplugins.com
+ * Version: 1.0.4.1
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wholesale-customers-for-woo
+ * Network: false
+ */
+
+/**
+ * 1.0.4.1 - 2017-11-08
+ * Bug Fix: If variations had same price for min and max value, display only one price instead of variation price.
+ * Enhancement: Subscribe to our newsletter prompt
+ */
+
+defined( 'ABSPATH' ) or exit;
+
+// include settings page.
+include( 'wholesale-customers-settings.php' );
+
+function wcs_apply_wholesale_pricing( $price, $product ) {
+	global $current_user;
+
+	//let's just get the price
+	$discount_price = floatval( $price );
+
+	$is_wholesale = get_user_meta( $current_user->ID, 'wcs_wholesale_customer', true );
+
+	if( $is_wholesale != '1' ) {
+		return $price;
+	}
+ 
+	// check to see if the current product has custom post meta, if it does don't apply the global discount.
+	$wholesale_price = get_post_meta( $product->get_id(), 'wholesale_price', true );
+
+	if( isset( $wholesale_price ) && $wholesale_price !== '' ) {
+		return $wholesale_price;
+	}
+
+	$wcs_global_discount = floatval( get_option( 'wcs_global_discount', true ) );
+
+	if( empty( $wcs_global_discount ) && $wcs_global_discount < 1 ){
+		return $discount_price;
+	}
+
+	$percentage = $wcs_global_discount / 100;
+
+	$discount_amount = $discount_price * $percentage;
+
+	$discount_price = $discount_price - $discount_amount;
+
+	//if the discounted price is below $0 set it to $0.
+		if( $discount_price <= 0 ){
+			$discount_price = 0;
+		}
+	
+	return $discount_price;
+}
+
+if ( !is_admin() || defined('DOING_AJAX') ) {    
+	add_filter("woocommerce_product_get_price", "wcs_apply_wholesale_pricing", 10, 2);
+	add_filter("woocommerce_product_variation_get_price", "wcs_apply_wholesale_pricing", 10, 2 );
+	add_filter("woocommerce_variable_price_html", "wcs_calculate_variation_range_prices", 10, 2);
+}
+
+function wcs_calculate_variation_range_prices( $variation_range_html, $product ) {
+	global $current_user;
+
+	$is_wholesale = get_user_meta( $current_user->ID, 'wcs_wholesale_customer', true );
+	$prices = $product->get_variation_prices( true );
+
+	if( !$is_wholesale ) {
+		$min_price     = current( $prices['price'] );
+		$max_price     = end( $prices['price'] );
+
+		return wc_format_price_range( $min_price, $max_price );
+	}
+
+	
+	// Check variation pricing for wholesale customers (individual pricing)
+	$product_id = $product->get_id();
+	$product_variables = new WC_Product_Variable( $product_id );
+	$variables = $product_variables->get_available_variations();
+
+	if( ! empty( $variables ) ) {
+
+		//we may need this later.
+		$wcs_global_discount = floatval( get_option( 'wcs_global_discount', true ) );
+
+		if( $wcs_global_discount ) {
+			$percentage = $wcs_global_discount / 100;
+		}
+	
+		$prices['wholesale_price'] = array();
+
+		foreach( $variables as $variation ) {
+
+			$id = $variation['variation_id'];
+
+			$wholesale_price = get_post_meta( $id, 'wholesale_price', true );
+
+				//if individual wholesale price is set for variation
+				if( isset( $wholesale_price ) && $wholesale_price !== '' ){
+
+					$prices['wholesale_price'][$id] = $wholesale_price;
+
+				}else{
+
+					// check to see if global discount is set and apply discount. ABSTRACT THIS TO A FUNCTION!!!!
+					if( $wcs_global_discount ) {
+						$discount = $prices['price'][$id] * $percentage;
+						$price_w_discount = $prices['price'][$id] - $discount;
+
+						//add to array now.
+						$prices['wholesale_price'][$id] =  sprintf( __( '%.2f', 'wholesale-customers-for-woo' ), $price_w_discount );
+
+					}else{
+						$prices['wholesale_price'][$id] = $prices['price'][$id];	
+					}	
+				}
+		}
+
+		if( ! empty( $prices['wholesale_price'] ) ) {
+
+		// Sort from low to high
+		asort( $prices['wholesale_price'] );
+
+		$wholesale_min_price = current( $prices['wholesale_price'] );
+		$wholesale_max_price = end( $prices['wholesale_price'] );
+
+		}
+		
+	}else{
+
+		$min_price     = current( $prices['price'] );
+		$max_price     = end( $prices['price'] );
+
+		
+		$wholesale_min_price = wcs_apply_wholesale_pricing( $min_price, $product );
+		$wholesale_max_price = wcs_apply_wholesale_pricing( $max_price, $product );	   
+
+	}
+
+	if( $wholesale_min_price == $wholesale_max_price ) {
+        
+        return wc_price( $wholesale_max_price );
+    
+    } else {
+    
+        return wc_format_price_range($wholesale_min_price, $wholesale_max_price);  
+    
+    }  
+	
+}
+
+/**
+ * Code for Minimum Cart Total.
+ * Taken from https://docs.woocommerce.com/document/minimum-order-amount/
+ * @since 1.0.1
+ */
+function wcs_minimum_cart_total(){
+	global $current_user;
+
+	$wholesale_customer = get_user_meta( $current_user->ID, 'wcs_wholesale_customer', true );
+
+	// Bail if customer isn't wholesale customer
+	if( ! $wholesale_customer ){
+		return;
+	}
+
+	// Get float for minimum cart amount
+	$minimum = floatval( get_option( 'wcs_min_cart_amount' ) );
+
+	if( $minimum === 0 || empty( $minimum ) ) {
+		return;
+	}
+
+	if ( WC()->cart->total < $minimum ) {	
+
+    	if( is_cart() ) {
+
+            wc_print_notice( 
+                sprintf( __( 'You must have an order with a minimum of %s to place your order, your current order total is %s.', 'wholesale-customers-for-woo' ) , 
+                    wc_price( $minimum ), 
+                    wc_price( WC()->cart->total )
+                ), 'error' 
+            );
+
+        } else {
+
+            wc_add_notice( 
+                sprintf( __( 'You must have an order with a minimum of %s to place your order, your current order total is %s.', 'wholesale-customers-for-woo' ) , 
+                    wc_price( $minimum ), 
+                    wc_price( WC()->cart->total )
+                ), 'error' 
+            );
+
+        }
+    }
+
+}
+
+add_action( 'woocommerce_checkout_process', 'wcs_minimum_cart_total' );
+add_action( 'woocommerce_before_cart' , 'wcs_minimum_cart_total' );
+
+
+function wcs_cost_product_field() {
+    woocommerce_wp_text_input( array( 'id' => 'wholesale_price', 'class' => 'wc_input_price short', 'label' => __( 'Wholesale price', 'wholesale-customers-for-woo' ) . ' (' . get_woocommerce_currency_symbol() . ')' ) );
+}
+
+add_action( 'woocommerce_product_options_pricing', 'wcs_cost_product_field' );
+
+function wcs_cost_save_product( $product_id ) {
+ 
+     // stop the quick edit interferring as this will stop it saving properly, when a user uses quick edit feature
+    if ( wp_verify_nonce($_POST['_inline_edit'], 'inlineeditnonce' ) ) {
+    	return;
+    }
+
+ 
+    // If this is a auto save do nothing, we only save when update button is clicked
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+
+	if ( isset( $_POST['wholesale_price'] ) && !empty( $_POST['wholesale_price'])) {
+		update_post_meta( $product_id, 'wholesale_price', $_POST['wholesale_price'] );
+	} else {
+		delete_post_meta( $product_id, 'wholesale_price' );
+	}
+}
+
+add_action( 'save_post', 'wcs_cost_save_product' );
+
+
+/**
+ * Add custom pricing fields to Variations.
+ *
+ * From http://www.remicorson.com/woocommerce-custom-fields-for-variations/
+ */
+
+function wcs_variation_settings_fields( $loop, $variation_data, $variation ) {
+	
+	woocommerce_wp_text_input( 
+		array( 
+			'id'          => 'wholesale_price[' . $variation->ID . ']', 
+			'label'       => __( 'Wholesale price (' . get_woocommerce_currency_symbol() . ')', 'wholesale-customers-for-woo' ), 
+			'desc_tip'    => 'true',
+			'description' => __( 'This price will be available to wholesale customers only. Overrites global discount.', 'wholesale-customers-for-woo' ),
+			'value'       => get_post_meta( $variation->ID, 'wholesale_price', true ),
+			'data_type'		  => 'price',
+		)
+	);
+
+}
+
+add_action( 'woocommerce_variation_options_pricing', 'wcs_variation_settings_fields', 5, 3 );
+
+// save custom fields for variations.
+function wcs_save_variation_settings_fields( $post_id ) { 
+
+	$number_field = $_POST['wholesale_price'][ $post_id ];
+
+	if( isset( $number_field ) ) {
+
+		if( intval($number_field) < 0 ){
+			$number_field = 0;
+		}
+		update_post_meta( $post_id, 'wholesale_price', esc_attr( $number_field ) );
+	}else{
+		delete_post_meta( $post_id, 'wholesale_price' );
+	}
+
+}
+add_action( 'woocommerce_save_product_variation', 'wcs_save_variation_settings_fields', 10, 1 );
+
+add_action( 'wp_ajax_wholesale_customers_woo_newsletter', 'wholesale_customers_woo_newsletter_callback' );
+
+function wholesale_customers_woo_newsletter_callback(){
+
+	if( isset( $_POST['action'] ) && $_POST['action'] == 'wholesale_customers_woo_newsletter' ){
+
+		if( isset( $_POST['email'] ) && $_POST['email'] != "" ){
+
+		  $request = wp_remote_post( 'https://yoohooplugins.com/api/mailing_list/subscribe.php', array( 'body' => array( 'action' => 'wholesale_customers_woo_newsletter', 'email' => $_POST['email'] ) ) );
+
+		  if( !is_wp_error( $request ) ){
+
+		    $request_body = wp_remote_retrieve_body( $request );
+
+		    if( $request_body == 'subscribed' ){
+		      	
+		      	$user = wp_get_current_user();
+
+				update_user_meta( $user->ID, 'wholesale_customers_newsletter_popup', 1 );
+
+				echo 1;
+
+		    }
+
+		  } else {
+
+		  }
+
+		} else {
+
+		  _e('Please enter in an email address to subscribe to our mailing list and receive your 20% coupon', 'wholesale-customers-for-woo');
+
+		}
+
+	}
+
+	wp_die();
+
+}
+
+add_action( 'admin_notices', 'wholesale_customers_woo_admin_notices' );
+
+function wholesale_customers_woo_admin_notices(){
+
+	$user = wp_get_current_user();
+
+	if( get_user_meta( $user->ID, 'wholesale_customers_newsletter_popup', true ) !== '1'){
+    	?>
+	        <div class="notice notice-success  wll-update-notice-newsletter is-dismissible" >
+		        <h3><?php _e('Wholesale Customers for Woo', 'wholesale-customers-for-woo'); ?></h3>
+		        <p><?php printf( __( 'Thank you for using Wholesale Customers for Woo. If you find this plugin useful please consider leaving a 5 star review %s.', 'wholesale-customers-for-woo' ), '<a href="https://wordpress.org/plugins/wholesale-customers-for-woo/#reviews" target="_blank">here</a>' ); ?></p>
+		        <p><?php _e( 'Sign up for our newsletter to get the latest product news and promotions, plus get 20% off of your next add-on purchase!', 'wholesale-customers-for-woo' ); ?> <?php printf( __('Browse through our add-ons %s', 'wholesale-customers-for-woo'), '<a href="https://yoohooplugins.com/plugins/?utm_source=plugin&utm_medium=wholesale_customers&utm_campaign=premium_addons" target="_BLANK">'.__('here', 'wholesale-customers-for-woo').'</a>' ); ?></p>
+		        <p><input type='email' style='width: 250px;' name='wll_user_subscribe_to_newsletter' id='wll_user_subscribe_to_newsletter' value='<?php echo $user->data->user_email; ?>' /><button class='button button-primary' id='wholesale_customers_subscribe'><?php _e('Subscribe Me!', 'wholesale-customers-for-woo'); ?></button></p>
+	        </div>
+        <?php
+	}
+
+}
+
+add_action( 'admin_enqueue_scripts', 'wholesale_customers_woo_admin_scripts' );
+
+function wholesale_customers_woo_admin_scripts(){
+
+	wp_enqueue_script( 'wholesale-customers-for-woo-admin-script', plugins_url( 'js/admin.js', __FILE__ ), array( 'jquery' ) );
+
+}
