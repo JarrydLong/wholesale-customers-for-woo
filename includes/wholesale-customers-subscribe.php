37dba61c02e472f5ee6afe90a24d3217323b1be5
Jarryd Long <JarrydLong@users.noreply.github.com>
2021-01-14 23:36:58 +0200

    Language changes to newsletter popup

--- includes/wholesale-customers-subscribe.php
+++ includes/wholesale-customers-subscribe.php
@@ -54,7 +54,9 @@ function wholesale_customers_woo_admin_notices(){
 	        <div class="notice notice-success  pps-update-notice-newsletter is-dismissible" >
 		        <h3><?php _e('Wholesale Customers for Woocommerce', 'wholesale-customers-for-woo'); ?></h3>
 		        <p><?php printf( __( 'Thank you for using Wholesale Customers for Woo. If you find this plugin useful please consider leaving a 5 star review %s.', 'wholesale-customers-for-woo' ), '<a href="https://wordpress.org/plugins/wholesale-customers-for-woo/#reviews" target="_blank">here</a>' ); ?></p>
-		        <p><?php _e( 'Sign up for our newsletter to get the latest product news and promotions, plus get 20% off of your next add-on purchase!', 'wholesale-customers-for-woo' ); ?> <?php printf( __('Browse through our plugins %s', 'wholesale-customers-for-woo'), '<a href="https://pacificplugins.com/?utm_source=plugin&utm_medium=wholesale_customers&utm_campaign=signup" target="_BLANK">'.__('here', 'wholesale-customers-for-woo').'</a>' ); ?></p>
+
+		        <p><?php printf( __( 'Sign up for our newsletter to get the latest product news and promotions, plus get 20&percnt; off the %s.', 'wholesale-customers-for-woo' ), '<a href="https://pacificplugins.com/downloads/wholesale-customers-for-woocommerce-pro/">Wholesale Customers for Woocommerce <strong>'.__('Pro Version', 'wholesale-customers-for-woo').'</strong></a>' ); ?></p>
+
 		        <p><input type='email' style='width: 250px;' name='pps_user_subscribe_to_newsletter' id='pps_user_subscribe_to_newsletter' value='<?php echo $user->data->user_email; ?>' /><button class='button button-primary' id='wholesale_customers_subscribe'><?php _e('Subscribe Me!', 'wholesale-customers-for-woo'); ?></button></p>
 	        </div>
         <?php
